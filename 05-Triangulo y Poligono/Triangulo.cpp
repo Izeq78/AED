7f9b9e4a7934955e07b882b68ef73f59a6b81412
Izeq78 <60624729+Izeq78@users.noreply.github.com>
2020-09-03 03:08:24 -0300

    Update Triangulo.cpp

--- 05-Triangulo y Poligono/Triangulo.cpp
+++ 05-Triangulo y Poligono/Triangulo.cpp
@@ -8,7 +8,7 @@ enum struct red {c0, c1, c2, c3, c4, c5, c6, c7, c8, c9};
 
 enum struct green {c0, c1, c2, c3, c4, c5, c6, c7, c8, c9}; 
 
-enum struct blue {c0, c1, c2, c3, c4, c5, c6, c7, c8, c9};      //utilizo los structs para limitar los valores de los colores a 10
+enum struct blue {c0, c1, c2, c3, c4, c5, c6, c7, c8, c9};      //utilizo los enums para limitar los valores de los colores a 10
 
 struct rgb {enum red;enum green; enum blue;};
 
